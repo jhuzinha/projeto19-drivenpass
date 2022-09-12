268d22e16741270fef3a98afdfbaf6569de862fc
Jhuliana <jholeanasantos@gmail.com>
2022-09-12 17:13:27 -0300

    token

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,7 +5,7 @@ import { IUsersType } from "../types/userType.js";
 export async function loginUser(req: Request, res: Response) {
     const user: IUsersType = req.body;
     const token = await userService.loginUser(user)
-    return res.status(200).send(token)
+    return res.status(200).send({ token })
 }
 
 export async function registerUser(req: Request, res: Response) {
